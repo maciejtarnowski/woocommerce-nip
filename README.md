b60c12f2bf3e104bfd057cbad5a163734227d175
Maciej Tarnowski <maciej@tarnowski.it>
2015-07-20 18:23:08 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,15 @@
+# woocommerce-nip
+
+Wtyczka dodaje pole NIP do ekranu szczegółów zamówienia w WooCommerce
+
+## Funkcje:
+- dodaje pole na NIP do szczegółów płatności
+- waliduje pole pod względem długości, numeryczności i poprawności sumy kontrolnej
+- zapisuje wartość pola w metadanych zamówienia
+- wyświetla wartość pola w szczegółach zamówienia w panelu
+
+## Uwaga
+Wymaga PHP w wersji 5.4+ ze względu na krótką notację tablic
+
+## Licencja
+[GNU General Public License Version 3](LICENSE.md)
---
+++ woocommerce-nip.php
@@ -0,0 +1,77 @@
+<?php
+/*
+Plugin Name: WooCommerce NIP
+Description: Dodaje pole na NIP do szczegółów płatności w WooCommerce
+Version:     1.0
+Author:      Maciej Tarnowski
+Author URI:  http://maxik.me
+License:     GPL3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+defined('ABSPATH') or die('Direct access not allowed');
+
+add_filter('woocommerce_checkout_fields', 'add_nip_billing_field');
+
+function add_nip_billing_field($fields)
+{
+    $fields['billing']['nip'] = [
+        'label'       => __('NIP', 'woocommerce'),
+        'placeholder' => __('Numer Identyfikacji Podatkowej', 'placeholder', 'woocommerce'),
+        'required'    => false,
+        'class'       => ['form-row-wide'],
+        'clear'       => true
+    ];
+
+    return $fields;
+}
+
+add_action('woocommerce_checkout_process', 'validate_nip_field', 10, 1);
+
+function validate_nip($nip)
+{
+    $nip = str_replace(['-', ' '], '', $nip);
+
+    if (strlen($nip) < 10 || !is_numeric($nip)) {
+        return false;
+    }
+
+    $nip = explode('', $nip);
+    $digits = array_map('intval', $nip);
+    $checksum = ((6 * $dig[0]) + (5 * $dig[1]) + (7 * $dig[2]) + (2 * $dig[3]) + (3 * $dig[4]) + (4 * $dig[5]) + (5 * $dig[6]) + (6 * $dig[7]) + (7 * $dig[8])) % 11;
+
+    if ($dig[9] == $checksum) {
+        return true;
+    }
+
+    return false;
+}
+
+function validate_nip_field()
+{
+    $nip = $_POST['nip'];
+
+    if (isset($nip) && !empty($nip) && !validate_nip($nip)) {
+        wc_add_notice(__('Proszę podać poprawny NIP'), 'error');
+    }
+}
+
+add_action('woocommerce_checkout_update_order_meta', 'save_nip_field', 10, 1);
+
+function save_nip_field($orderId)
+{
+    $nip = $_POST['nip'];
+
+    if (!isset($nip) || !validate_nip($nip)) {
+        return;
+    }
+
+    update_post_meta($orderId, 'nip', sanitize_text_field($nip));
+}
+
+add_action('woocommerce_admin_order_data_after_billing_address', 'add_nip_to_billing_info', 10, 1);
+
+function add_nip_to_billing_info($order)
+{
+    echo '<p><strong>' . __('NIP') . ':</strong> ' . (get_post_meta($order->id, 'nip', true) ? : 'brak') . '</p>';
+}
